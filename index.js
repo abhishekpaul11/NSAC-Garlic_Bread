264d114134bc911fef92da1db248344c104f03fb
SwaroopNandy2109 <swaroopnandy2000@gmail.com>
2020-10-03 19:22:40 +0530

    Layers

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let map;
+
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {
@@ -11,6 +12,15 @@ function initMap() {
         minZoom: 2
     };
     map.setOptions(opt);
+    var landcover = new google.maps.ImageMapType({
+        getTileUrl: getTileUrl,
+        name: "Landcover",
+        alt: "National Land Cover Database 2016",
+        minZoom: 0,
+        maxZoom: 19,
+        opacity: 1.0
+    });
+    map.overlayMapTypes.push(landcover);
     $(document).ready(function() {
         $.ajax({
             type: "GET",
@@ -25,6 +35,31 @@ function initMap() {
         });
     });
 }
+
+var EXTENT = [-Math.PI * 6378137, Math.PI * 6378137];
+
+function xyzToBounds(x, y, z) {
+    var tileSize = (EXTENT[1] * 2) / Math.pow(2, z);
+    var minx = EXTENT[0] + x * tileSize;
+    var maxx = EXTENT[0] + (x + 1) * tileSize;
+    // remember y origin starts at top
+    var miny = EXTENT[1] - (y + 1) * tileSize;
+    var maxy = EXTENT[1] - y * tileSize;
+    return [minx, miny, maxx, maxy];
+}
+
+var getTileUrl = function(coordinates, zoom) {
+    return (
+        "https://www.mrlc.gov/geoserver/NLCD_Land_Cover/wms?" +
+        "&REQUEST=GetMap&SERVICE=WMS&VERSION=1.1.1" +
+        "&LAYERS=mrlc_display%3ANLCD_2016_Land_Cover_L48" +
+        "&FORMAT=image%2Fpng" +
+        "&SRS=EPSG:3857&WIDTH=256&HEIGHT=256" +
+        "&BBOX=" +
+        xyzToBounds(coordinates.x, coordinates.y, zoom).join(",")
+    );
+};
+
 setMarker = function(lat, lng) {
     lat = Number(lat)
     lng = Number(lng)
